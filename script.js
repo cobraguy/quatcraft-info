97e0ad8f5e61b75f19d75e91982f6f5568670085
cobraguy <cobraguy@users.noreply.github.com>
2016-04-07 11:15:23 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,41 @@
+var serverRequest = new XMLHttpRequest(),
+	serverInfo;
+
+serverRequest.open("GET", "http://api.minetools.eu/ping/quatcraft.mcnode.net/25565", true);
+serverRequest.onreadystatechange = function(){
+	if(serverRequest.readyState === XMLHttpRequest.DONE && serverRequest.status === 200){
+		serverInfo = JSON.parse(serverRequest.responseText);
+		loadData();
+	}
+};
+serverRequest.send();
+
+function loadData(){
+	var playerInfo = serverInfo.players;
+	document.getElementById("server-icon").setAttribute("src", serverInfo.favicon);
+	document.getElementById("version").textContent = serverInfo.version.name;
+	document.getElementById("server-description").textContent = serverInfo.description;
+	document.getElementById("amount-of-players").textContent = playerInfo.online + "/" + playerInfo.max;
+	if(playerInfo.online > 0){
+		for(var i = 0; i < playerInfo.online; i++){
+			var container = document.createElement("div");
+			container.className = "player";
+			
+			var icon = document.createElement("img");
+			icon.setAttribute("src", "https://crafatar.com/avatars/" + playerInfo.sample[i].id + "?size=40&overlay");
+			
+			var playerName = document.createElement("p");
+			playerName.textContent = playerInfo.sample[i].name;
+			
+			container.appendChild(icon);
+			container.appendChild(playerName);
+			document.getElementById("player-list").appendChild(container);
+		}
+	}else{
+		var noPlayers = document.createElement("p");
+		noPlayers.textContent = "No players online";
+		document.getElementById("player-list").appendChild(noPlayers);
+	}
+	
+	document.getElementById("loading-label").textContent = "QuatCraft";
+}
